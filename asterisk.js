5fbc850abb811b8ee535ba8443fcd27545e6ee42
Titus Herdiawan <titus@Tituss-MacBook-Pro.local>
2022-12-17 21:01:19 +0700

    create a asterisk tree

--- asterisk.js
+++ asterisk.js
@@ -1,70 +1,79 @@
-// console.log("WHILE");
-// let rows1 = 5;
-// while (rows1 > 0) {
-//   console.log("*");
-//   rows1--;
-// }
+console.log("WHILE");
+let rows1 = 5;
+while (rows1 > 0) {
+  console.log("*");
+  rows1--;
+}
 
+let rows2 = 5;
 // let rows2 = 5;
-// // let rows2 = 5;
-// while (rows2 > 0) {
-//   let temp = "";
-//   let i = 5;
-//   while (i > 0) {
-//     temp += "*";
-//     i--;
-//   }
-//   console.log(temp);
-//   rows2--;
-// }
+while (rows2 > 0) {
+  let temp = "";
+  let i = 5;
+  while (i > 0) {
+    temp += "*";
+    i--;
+  }
+  console.log(temp);
+  rows2--;
+}
 
-// let rows3 = 5;
-// let i = 0;
-// let j = 1;
-// let temp = "";
-// while (rows3 > 0) {
-//   // console.log(i);
-//   while (i < j) {
-//     temp += "*";
-//     console.log(temp);
-//     i++;
-//   }
-//   j++;
-//   rows3--;
-// }
+let rows3 = 5;
+let i = 0;
+let j = 1;
+let temp = "";
+while (rows3 > 0) {
+  // console.log(i);
+  while (i < j) {
+    temp += "*";
+    console.log(temp);
+    i++;
+  }
+  j++;
+  rows3--;
+}
 
-// let rows4 = 5;
+let rows4 = 5;
 
-// while (rows4 > 0) {
-//   let k = rows4;
-//   console.log(rows4);
-//   let temp = "";
-//   while (k > 0) {
-//     temp += "*";
-//     k--;
-//   }
-//   console.log(temp);
-//   rows4--;
-// }
+while (rows4 > 0) {
+  let k = rows4;
+  console.log(rows4);
+  let temp = "";
+  while (k > 0) {
+    temp += "*";
+    k--;
+  }
+  console.log(temp);
+  rows4--;
+}
 
-// for (let i = rows4; i > 0; i--) {
-//   let temp = "";
-//   for (let j = 0; j < i; j++) {
-//     temp += "*";
-//   }
-//   console.log(temp);
-// }
+for (let i = rows4; i > 0; i--) {
+  let temp = "";
+  for (let j = 0; j < i; j++) {
+    temp += "*";
+  }
+  console.log(temp);
+}
 
 /*
 # # * # #
 # * * * #
 * * * * * 
 */
-let temp = "";
-for (var i = 0; i < 8; i++) {
-  for (var j = 0; j <= i; j++) {
-    temp += "^";
+
+let row7 = 5;
+for (let i = 1; i <= row7; i++) {
+  let temp = "";
+  let j = 0;
+  while (j < row7 - i) {
+    temp += " ";
+    j++;
   }
-  temp += "<br>";
+  j = 0;
+  while (j < i) {
+    temp += "*";
+    j++;
+  }
+  console.log(temp);
 }
 
--- test.js
+++ test.js
@@ -1,22 +1,42 @@
-let word = 'developer'
-let wordArr = [];
-    // console.log(wordArr);
-    
-    // create new array
-    for(let i = 0; i < word.length; i++){
-        wordArr.push(word[i]);
-    }
-    console.log("sajfda",wordArr);
+// let word = 'developer'
+// let wordArr = [];
+// console.log(wordArr);
 
-   
-    let newArr = [];
-    for(let j = 0; j < wordArr.length; j++){
-        console.log(wordArr.length);
-        let temp = [];
-        for(let k = 0; k < wordArr.length; k++){
-            temp.push(word[k]);
-        }
-        newArr.push(temp);
-        console.log(`ini word ${wordArr}`);
-        wordArr.pop();
+// create new array
+// for(let i = 0; i < word.length; i++){
+//     wordArr.push(word[i]);
+// }
+// console.log("sajfda",wordArr);
+
+// let newArr = [];
+// for(let j = 0; j < wordArr.length; j++){
+//     console.log(wordArr.length);
+//     let temp = [];
+//     for(let k = 0; k < wordArr.length; k++){
+//         temp.push(word[k]);
+//     }
+//     newArr.push(temp);
+//     console.log(`ini word ${wordArr}`);
+//     wordArr.pop();
+// }
+
+let dice = Math.ceil(Math.random() * 6);
+
+while (true){
+    console.log(dice);
+    if(dice === 6){
+        break;
+    } else {
+        dice = Math.ceil(Math.random() * 6);
     }
+}
+
+// console.log();
+for(;true;){
+    console.log(dice);
+    if(dice === 6){
+        break;
+    } else {
+        dice = Math.ceil(Math.random() * 6);
+    }
+}
